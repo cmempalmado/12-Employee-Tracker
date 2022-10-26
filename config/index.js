ae7471b08eb4f4e4467526f050cb6d590eaecca2
cmempalmado <cmempalmado@gmail.com>
2022-10-26 04:48:11 -0700

    fixed env Example

--- config/index.js
+++ config/index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 
 const db = mysql.createConnection (
     {
-        host: process.env.DB_HOST,
+        host: 'localhost',
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
